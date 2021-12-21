7c02ac4f2171ab92cd60199fc855f93a90ca52d6
Severin Ibarluzea <seveibar@gmail.com>
2021-12-21 09:56:52 -0800

    fix close bug

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const { createServer } = require("http")
 const next = require("next")
+const path = require("path")
 
 module.exports = async ({ port }) => {
-  const app = next({
+  const customServerConf = {
     dev: false,
+    dir: path.resolve(__dirname),
     quiet: false,
-  })
+  }
+  console.log({ customServerConf })
+  const app = next(customServerConf)
   const handleRequest = app.getRequestHandler()
   await app.prepare()
   const server = createServer((req, res) => {
     return handleRequest(req, res)
   })
+  console.log("nextjs-health-endpoint-module listening on", port)
   server.listen(port)
-  return { close: server.close() }
+  return { close: () => server.close() }
 }
 
 if (!module.parent) {
